2a5e1c3c4048b710637d8bb8436bf5a9e7522d95
srv <SelVBN2018@gmail.com>
2019-09-12 21:58:17 +0530

    modified paragraph for release1

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React quicker now !. 
+          Learn React quicker now !. Added in release1 . 
         </a>
       </header>
     </div>
